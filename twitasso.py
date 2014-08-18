df1ef75f1996ae637bd2795b6b9a7ca26649e26f
Sérgio Berlotto Jr <sergio.berlotto@gmail.com>
2014-08-17 21:25:07 -0300

    Ajuste para autorizar o usuario via twitter

--- twitasso.py
+++ twitasso.py
@@ -1,20 +1,51 @@
+# -*- encoding: utf-8 -*-
+
 from twython import Twython
 import time
+import twittasso_config
+from flask import Flask, redirect, render_template, abort, request, escape, session
+import webbrowser as browser
+import threading
 
-tempo = 60
-app_key = "5Llo6LA1DldnAdUrgAF5A"
-app_secret = "Bvh8NlRXpWZz3ZsOcUelGOznm3afectdMJ1Wgkk"
-oauth_token = "22940852-NDwTnkRe7itGdhEHWFT5CuAFbQm3tcjc5bGjStA3F"
-oauth_token_secret = "zL1XJpJCWYPGLZV6gotdgqHsea8QT8L6x0l4mFno"
+CALLBACK_URL = 'http://localhost:5000/callback'
+APP_KEY = "XjOBXjo4Cv3LXQcO3ZfA"
+APP_SECRET = "CyVor32vCki847fvskGjIFtfeXnptDEoyCDGPJNto"
+TIME = 60 #In seconds
 
-t = Twython(app_key, app_secret, oauth_token, oauth_token_secret)
+t = Twython(APP_KEY, APP_SECRET)
 
-# print t.get_home_timeline()
+auth = t.get_authentication_tokens()
+OAUTH_TOKEN = auth['oauth_token']
+OAUTH_TOKEN_SECRET = auth['oauth_token_secret']
+
+browser.open(auth['auth_url'])
+
+print "If your browser not open, then open a new tab and navigate to: %s" % auth['auth_url']
+
+THE_PIN = raw_input("Write the PIN returned in your browser:")
+
+twitter = Twython(APP_KEY, APP_SECRET,
+					OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
+
+final_step = twitter.get_authorized_tokens(THE_PIN)
+
+FINAL_OAUTH_TOKEN = final_step['oauth_token']
+FINAL_OAUTH_TOKEN_SECRET = final_step['oauth_token_secret']
+
+twitter = Twython(APP_KEY, APP_SECRET,
+                  FINAL_OAUTH_TOKEN, FINAL_OAUTH_TOKEN_SECRET)
+
+credencials = twitter.verify_credentials()
+
+print "Hello %s! You are '%s' in Twitter, and now I'll twiit for you..." % (credencials['name'], credencials['screen_name'])
 
 try:
     f = open('twits.txt')
     for line in f:
-        t.update_status(status=line)
-        time.sleep(tempo)
+    	print "Twitando: %s" % line
+        twitter.update_status(status=line)
+        time.sleep(TIME)
 except TwythonError as e:
     print e
+
+print "Concluído!"
